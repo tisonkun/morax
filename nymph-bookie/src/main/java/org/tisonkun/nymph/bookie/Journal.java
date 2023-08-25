908a7caa4e4ed11e07c755f3f053bc70f8a42b85
tison <wander4096@gmail.com>
2023-08-26 00:54:42 +0800

    initialize
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ nymph-bookie/src/main/java/org/tisonkun/nymph/bookie/Journal.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 tison <wander4096@gmail.com>
+ * All rights reserved.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.tisonkun.nymph.bookie;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Journal {
+
+    public static void main(String[] args) {
+        log.info("Hello world!");
+    }
+}
