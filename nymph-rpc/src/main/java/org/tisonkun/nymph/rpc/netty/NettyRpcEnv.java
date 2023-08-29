7b57cf88815db38ce22fc702c57bf901c773e1ed
tison <wander4096@gmail.com>
2023-08-29 15:29:03 +0800

    style
    Signed-off-by: tison <wander4096@gmail.com>

--- nymph-rpc/src/main/java/org/tisonkun/nymph/rpc/netty/NettyRpcEnv.java
+++ nymph-rpc/src/main/java/org/tisonkun/nymph/rpc/netty/NettyRpcEnv.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.tisonkun.nymph.rpc.netty;
 
 import com.google.common.base.Preconditions;
@@ -181,7 +200,7 @@ public class NettyRpcEnv extends RpcEnv {
     private <T> T deserialize(TransportClient client, ByteBuffer bytes) {
         return (T) NettyRpcEnv.CURRENT_CLIENT.withValue(client, () -> {
             try (final ByteBufferInputStream bis = new ByteBufferInputStream(bytes);
-                 final ObjectInputStream ois = new ObjectInputStream(bis)) {
+                    final ObjectInputStream ois = new ObjectInputStream(bis)) {
                 return ois.readObject();
             }
         });
