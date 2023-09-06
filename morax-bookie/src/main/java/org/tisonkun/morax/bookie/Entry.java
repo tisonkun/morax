0e750a769bd12cc6472b7cb78f417b3c2e0076ae
tison <wander4096@gmail.com>
2023-09-06 22:26:18 +0800

    simple abstractions
    Signed-off-by: tison <wander4096@gmail.com>

--- morax-bookie/src/main/java/org/tisonkun/morax/bookie/Entry.java
+++ morax-bookie/src/main/java/org/tisonkun/morax/bookie/Entry.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 tison <wander4096@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.tisonkun.morax.bookie;
 
 import io.netty.buffer.ByteBuf;
@@ -12,30 +28,37 @@ public class Entry {
     private final long lastConfirmed;
     private final ByteBuf payload;
 
+    private transient volatile ByteBuf cachedBytes;
+
     public static Entry of(ByteBuf entry) {
         final ByteBuf payload = entry.duplicate();
         final long ledgerId = payload.readLong();
         final long entryId = payload.readLong();
         final long lastConfirmed = payload.readLong();
-        return new Entry(ledgerId, entryId, lastConfirmed, payload);
+        final Entry result = new Entry(ledgerId, entryId, lastConfirmed, payload);
+        result.cachedBytes = entry.duplicate();
+        return result;
     }
 
     public ByteBuf toBytes() {
+        if (cachedBytes != null) {
+            return cachedBytes;
+        }
         final ByteBuf result = Unpooled.buffer(8 + 8 + 8 + payload.readableBytes());
         result.writeLong(ledgerId);
         result.writeLong(entryId);
         result.writeLong(lastConfirmed);
         result.writeBytes(payload, payload.readerIndex(), payload.readableBytes());
+        cachedBytes = result;
         return result;
     }
 
     @Override
     public String toString() {
-        return "Entry{" +
-                "ledgerId=" + ledgerId +
-                ", entryId=" + entryId +
-                ", lastConfirmed=" + lastConfirmed +
-                ", payload=" + payload.toString(StandardCharsets.UTF_8) +
-                '}';
+        return "Entry{" + "ledgerId="
+                + ledgerId + ", entryId="
+                + entryId + ", lastConfirmed="
+                + lastConfirmed + ", payload="
+                + payload.toString(StandardCharsets.UTF_8) + '}';
     }
 }
